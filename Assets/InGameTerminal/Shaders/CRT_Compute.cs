0df99f779ac8a9523f1ad4c9f1a3b50347bc3105
jimm <jimm@jimm.horse>
2026-01-19 16:39:09 -0500

    wip

--- Assets/InGameTerminal/Shaders/CRT_Compute.cs
+++ Assets/InGameTerminal/Shaders/CRT_Compute.cs
@@ -16,10 +16,50 @@ namespace InGameTerminal.Shaders
 		[Range(0.0f, 1.0f)]
 		private float decay = 1.0f;
 		private int kernel;
+		private int kernel2;
 		private RenderTexture buffer;
+		private RenderTexture buffer2;
 		private RenderTexture input;
+
+
+/*
+    Properties
+    {
+        _MainTex ("Source", 2D) = "white" {}
+        _BaseRadius ("Base Radius (pixels)", Range(0, 10)) = 0
+        _FwidthFactor ("Fwidth -> Radius", Range(0, 200)) = 40
+        _MaxRadius ("Max Radius (pixels)", Range(0, 32)) = 12
+        _Quality ("Quality (0=fast, 1=better)", Range(0, 1)) = 1
+    }
+*/
+		private Shader adaptiveBlur;
+		private Material adaptiveBlurMaterial;
+		private RenderTexture adaptiveBlurRenderTexture;
+		[Range(0.0f, 10.0f)]
+		public float adaptiveBlur_baseRadius = 0.0f;
+		[Range(0.0f, 200.0f)]
+		public float adaptiveBlur_fwidthFactor = 40.0f;
+		[Range(0.0f, 32.0f)]
+		public float adaptiveBlur_maxRadius = 12.0f;
+		[Range(0.0f, 1.0f)]
+		public float adaptiveBlur_quality = 1.0f;
+		// _DistanceScale
+		[Range(0.0f, 2000.0f)]
+		public float adaptiveBlur_distanceScale = 100.0f;
+
 		private void OnEnable()
 		{
+			adaptiveBlur = Shader.Find("InGameTerminal/FWidthAdaptiveBlur");
+			if (adaptiveBlur != null)
+			{
+				adaptiveBlurMaterial = new Material(adaptiveBlur);
+			}
+			else
+			{
+				Debug.LogError("Adaptive Blur Shader not found!", this);
+			}
+			buffer = null;
+			input = null;
 			if (computeShader == null)
 			{
 				Debug.LogError("ComputeShader is not assigned!", this);
@@ -28,32 +68,29 @@ namespace InGameTerminal.Shaders
 			}
 
 			kernel = computeShader.FindKernel("CSMain");
-			Debug.Log($"Kernel index: {kernel}", this);
-		}
-		private void Start()
-		{
-			//material = new Material(computeShader);
+			kernel2 = computeShader.FindKernel("CSMain2");
+			Debug.Log($"Kernel index: {kernel} {kernel2}", this);
 		}
 		private void Update()
 		{
-			//RenderTexture currentSource = renderTexture;
-			//RenderTexture temp = RenderTexture.GetTemporary(currentSource.width, currentSource.height, 0, currentSource.format);
-			//Graphics.Blit(currentSource, temp, material);
-			//if (currentSource != renderTexture)
-			//{
-			//	RenderTexture.ReleaseTemporary(currentSource);
-			//}
-
-			// Set texture before each dispatch
-			//computeShader.SetTexture(kernel, "_UIInput", renderTexture);
-			//this.computeShader.Dispatch(kernel, renderTexture.width / 8, renderTexture.height / 8, 1);
-
-			if (input != null)
+			if (input && buffer && buffer2)
 			{
 				computeShader.SetTexture(kernel, "_UIInput", input);
 				computeShader.SetFloat("_decay", Time.deltaTime*(1.0f/decay));
 				computeShader.SetTexture(kernel, "_Buffer", buffer);
-				this.computeShader.Dispatch(kernel, input.width / 8, input.height / 8, 1);
+				computeShader.SetTexture(kernel2, "_Buffer", buffer);
+				computeShader.SetTexture(kernel2, "_Buffer2", buffer2);
+				//this.computeShader.Dispatch(kernel, input.width / 8, input.height / 8, 1);
+				this.computeShader.Dispatch(kernel, 1, 24, 1);
+				this.computeShader.Dispatch(kernel2, input.width / 8, input.height / 8, 1);
+			}
+			if (adaptiveBlurMaterial)
+			{
+				adaptiveBlurMaterial.SetFloat("_BaseRadius", adaptiveBlur_baseRadius);
+				adaptiveBlurMaterial.SetFloat("_FwidthFactor", adaptiveBlur_fwidthFactor);
+				adaptiveBlurMaterial.SetFloat("_MaxRadius", adaptiveBlur_maxRadius);
+				adaptiveBlurMaterial.SetFloat("_Quality", adaptiveBlur_quality);
+				adaptiveBlurMaterial.SetFloat("_DistanceScale", adaptiveBlur_distanceScale);
 			}
 		}
 		public void OnRenderImage(RenderTexture inTexture, RenderTexture outTexture)
@@ -70,8 +107,42 @@ namespace InGameTerminal.Shaders
 				buffer = new RenderTexture(inTexture.width, inTexture.height, 0, RenderTextureFormat.ARGB32);
 				buffer.enableRandomWrite = true;
 				buffer.Create();
+
+				var prev = RenderTexture.active;
+				RenderTexture.active = buffer;
+				GL.Clear(true, true, Color.black); // Color.black is (0,0,0,1)
+				RenderTexture.active = prev;
 			}
-			Graphics.Blit(buffer, outTexture);
+			if (!buffer2)
+			{
+				buffer2 = new RenderTexture(inTexture.width, inTexture.height, 0, RenderTextureFormat.ARGB32);
+				buffer2.enableRandomWrite = true;
+				buffer2.Create();
+
+				var prev = RenderTexture.active;
+				RenderTexture.active = buffer;
+				GL.Clear(true, true, Color.black); // Color.black is (0,0,0,1)
+				RenderTexture.active = prev;
+			}
+			// run adaptiveBlur on buffer2
+
+			if (adaptiveBlurMaterial)
+			{
+				if (!adaptiveBlurRenderTexture)
+				{
+					adaptiveBlurRenderTexture = new RenderTexture(inTexture.width, inTexture.height, 0, RenderTextureFormat.ARGB32);
+					adaptiveBlurRenderTexture.enableRandomWrite = true;
+					adaptiveBlurRenderTexture.Create();
+				}
+				Graphics.Blit(buffer2, adaptiveBlurRenderTexture, adaptiveBlurMaterial);
+				Graphics.Blit(adaptiveBlurRenderTexture, outTexture);
+				//Debug.Log("Applied adaptive blur", this);
+			}
+			else
+			{
+				Graphics.Blit(buffer2, outTexture);
+				//Debug.Log("Skipped adaptive blur", this);
+			}
 		}
 	}
 }
---
+++ Assets/InGameTerminal/Shaders/FWidthAdaptiveBlur.shader
@@ -0,0 +1,71 @@
+Shader "InGameTerminal/FWidthAdaptiveBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _DistanceScale ("Distance Scale", Range(1, 2000)) = 100.0
+		_CameraPos ("Camera Position", Vector) = (0,0,0,0)
+    }
+    
+    SubShader
+    {
+        Tags
+        {
+            "Queue" = "Transparent"
+            "IgnoreProjector" = "True"
+            "RenderType" = "Transparent"
+            "PreviewType" = "Plane"
+            "DisableBatching" = "True"
+        }
+        
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+        
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+            
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+            
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float3 worldPos : TEXCOORD1;
+				float dist : TEXCOORD2;
+            };
+            
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _DistanceScale;
+            
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;
+				o.dist = distance(_WorldSpaceCameraPos, o.worldPos);
+                return o;
+            }
+            
+            fixed4 frag(v2f i) : SV_Target
+            {float d = distance(_WorldSpaceCameraPos, i.worldPos);
+return float4(frac(d * 0.01), 0, 0, 1);
+
+                return fixed4(i.dist/10000.0f, 0, 0, 1);
+            }
+            ENDCG
+        }
+    }
+}
